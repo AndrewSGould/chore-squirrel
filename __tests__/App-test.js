cb97e764d3c69b726b49e091017e18aa7980308a
AndrewSGould <agould743@gmail.com>
2020-05-09 14:30:50 -0400

    update out-of-box broken test

--- __tests__/App-test.js
+++ __tests__/App-test.js
@@ -1,29 +1,28 @@
-import * as React from 'react';
-import NavigationTestUtils from 'react-navigation/NavigationTestUtils';
-import renderer from 'react-test-renderer';
+import * as React from "react";
+import { act, create } from "react-test-renderer";
+import App from "../App";
 
-import App from '../App';
-
-jest.mock('expo', () => ({
-  AppLoading: 'AppLoading',
+jest.mock("expo", () => ({
+  Linking: {
+    makeUrl: () => "/",
+  },
+  SplashScreen: {
+    preventAutoHide: () => "preventAutoHide",
+    hide: () => "hide",
+  },
 }));
 
-jest.mock('../navigation/AppNavigator', () => 'AppNavigator');
+jest.mock("../navigation/BottomTabNavigator", () => "BottomTabNavigator");
 
-describe('App', () => {
+describe("App", () => {
   jest.useFakeTimers();
 
-  beforeEach(() => {
-    NavigationTestUtils.resetInternalState();
-  });
-
-  it(`renders the loading screen`, () => {
-    const tree = renderer.create(<App />).toJSON();
-    expect(tree).toMatchSnapshot();
-  });
-
-  it(`renders the root without loading screen`, () => {
-    const tree = renderer.create(<App skipLoadingScreen />).toJSON();
-    expect(tree).toMatchSnapshot();
+  let tree;
+  it(`renders the loading screen`, async () => {
+    // act is used to prevent snapshot returning null
+    await act(async () => {
+      tree = await create(<App />);
+    });
+    expect(tree.toJSON()).toBeTruthy();
   });
 });
